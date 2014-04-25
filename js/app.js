2ff34372d1cf739cde9b8806b593ce38362a880b
mhkeller <michael.keller@gmail.com>
2014-04-25 16:40:22 -0400

    scaling working with absolute positioning

--- js/app.js
+++ js/app.js
@@ -1,43 +1,38 @@
 (function(){
 	'use strict';
 
-	var CONFIG = {
-		full_width: 570 // This is the full width-pixel value of the image, #pages width should match this
-	}
-
 	var states = {
 		zoom: 'page',
 		currentPage: '1',
 		currentHotspot: 'none',
 		lastHotspot: '',
-		transition: {
-			enabled: true,
-			duration: '.3s'
-		}
+		transitionDuration: '350ms'
 	}
 
 	var helpers = {
-		setTransitionCss: function(property, value){
-			var css = helpers.vendorPrefix(property, value);
-			css = helpers.addDuration(css)
+		setTransitionCss: function(property, value, transitionDuration){
+			var css = {}, duration = 0;
+			css[property] = value;
+			css = helpers.addDuration(css, transitionDuration);
 			return css;
 		},
-		vendorPrefix: function(property, value, obj){
-			var pfxs = ['webkit', 'moz', 'ms', 'o'];
-			obj = obj || {};
-			for (var i = 0; i < pfxs.length; i++){
-				obj['-' + pfxs[i] + '-' + property] = value
-			}
-			return obj;
-		},
-		addDuration: function(cssObj){
-			var duration = states.transition.enabled ? states.transition.duration : 0;
-			cssObj = helpers.vendorPrefix('transition-duration', duration, cssObj);
+		addDuration: function(cssObj, transitionDuration){
+			var duration = transitionDuration ? states.transitionDuration : 0
+			_.extend(cssObj, {'transition-duration': duration});
 			return cssObj;
 		},
+		// vendorPrefix: function(property, value, obj){
+		// 	var pfxs = ['webkit', 'moz', 'ms', 'o'];
+		// 	obj = obj || {};
+		// 	for (var i = 0; i < pfxs.length; i++){
+		// 		obj['-' + pfxs[i] + '-' + property] = value
+		// 	}
+		// 	return obj;
+		// },
 		saveCurrentStates: function(page, hotspot){
 			states.currentPage = page;
 			states.currentHotspot = hotspot;
+			// Maybe save last hotspot here
 		},
 		hashToPageHotspotDict: function(hash){
 			// If for some reason it has a slash as the last character, cut it so as to not mess up the split
@@ -74,12 +69,14 @@
 		},
 		bakePages: function(pages){
 			var page_markup, $page;
-			for (var i = 0; i < 1; i++){
+			for (var i = 0; i < pages.length; i++){
 				page_markup = templates.pageFactory(pages[i]);
 				$('#pages').append(page_markup);
 				$page = $('#page-'+pages[i].number);
-				layout.measureHotspots( $page ); // For zooming, we need to know the absolute location of each hotspot so we can know how to get to it
+
+				layout.measurePage( $page ); // For zooming, we need to know the absolute location of each hotspot so we can know how to get to it
 				listeners.hotspotClicks( $page );
+				listeners.pageTransitions();
 			}
 			routing.initRoute();
 		},
@@ -92,30 +89,33 @@
 				$hs.attr('data-height', $hs.height() );
 			});
 		},
+		measureImgSetPageHeight: function($page){
+			var img_height = $page.find('img').height();
+			$('#pages').css('height', img_height+'px');
+		},
+		measurePage: function ($page){
+			layout.measureImgSetPageHeight($page);
+			layout.measureHotspots($page);
+		},
 		update: function(){
 			// Do this on window resize
 			var current_page = 1;
 			var $page = $('#page-'+current_page);
-
-			// Disable transitions so it happens quickly
-			states.transition.enabled = false;
-			// Scale the page back down to 1x1
-			zooming.reset(current_page);
+			// Scale the page back down to 1x1, ($page, masksAlso, transitionDuration)
+			zooming.toPage($page, true, false);
 			// Measure the page at those dimensions
-			layout.measureHotspots( $page );
+			layout.measurePage( $page );
 			// Get what page and hotspot we're on
 			var page_hotspot = helpers.hashToPageHotspotDict(window.location.hash);
 			// And initiate zooming to that hotspot
-			routing.read(page_hotspot.page, page_hotspot.hotspot);
-			// Re-enable transitions for next time we click or swipe
-			states.transition.enabled = true;
+			routing.read(page_hotspot.page, page_hotspot.hotspot, false);
 		}
 	}
 
 	var listeners = {
 		resize: function(){
 			layout.updateDebounce = _.debounce(layout.update, 300);
-			// Does this trigger on different mobile device orientation changes?
+			// TODO Does this trigger on different mobile device orientation changes?
 			window.addEventListener('resize', function(){
 				layout.updateDebounce();
 			})
@@ -140,6 +140,31 @@
 				var direction = helpers.getNavDirection(e, 'swiperight');
 				routing.set.fromKeyboardOrGesture(direction);
 			});
+		},
+		pageTransitions: function(){
+			$(".page").on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', function(){
+
+				$('.page').removeClass('enter-from-left')
+									.removeClass('enter-from-right')
+									.removeClass('exit-to-left')
+									.removeClass('exit-to-right');
+
+				$('.page').each(function(index, el){
+					zooming.toPage( $(el) );
+				})
+			});
+		}
+	}
+
+	var paging = {
+		nextPage: function(){
+
+		},
+		prevPage: function(){
+
+		},
+		goToPage: function(){
+
 		}
 	}
 
@@ -155,10 +180,10 @@
 			routing.router = new routing.Router;
 
 			routing.router.on('route:page', function(page) {
-				routing.read(page);
+				routing.read(page, null, true);
 			});
 			routing.router.on('route:hotspot', function(page, hotspot) {
-				routing.read(page, hotspot);
+				routing.read(page, hotspot, true);
 			});
 				
 			// For bookmarkable Urls
@@ -188,29 +213,37 @@
 				var pp_info = helpers.hashToPageHotspotDict( window.location.hash );
 				var page_max = Number( $('#page-'+pp_info.page).attr('data-length') );
 				var newhash;
+				pp_info.page = pp_info.page || 1; // If there's no page, go to the first page
+				pp_info.hotspot = pp_info.hotspot || states.lastHotspot || 0; // If there was no hotspot in the hash, see if there was a saved hotspot states, if not start at zero
 
-				// TODO interpage navigation if its last or first
-
-				// If there was no hotspot, see if there was a saved hotspot states, if not start at zero
-				pp_info.hotspot = pp_info.hotspot || states.lastHotspot || 0;
+				states.lastHotspot = pp_info.hotspot;
+				// Go to previous hotspot
 				if (direction == 'prev-hotspot'){
+					// Decrease our hotspot cursor by one
 					pp_info.hotspot--
 
-					// If it's below one, go to the full view of this page
+					// If that takes us below the first hotspot, go to the full view of this page
 					if (pp_info.hotspot < 1){
+						if (pp_info.page != 1) pp_info.page--;
+						// TODO handle first page to go back to main window or something
 						states.lastHotspot = '';
 						pp_info.hotspot = 'none';
 					}
 
+				// Go to next hotspot
 				} else if (direction == 'next-hotspot'){
+					// Increase our hotspot cursor by one
 					pp_info.hotspot++;
 
-					// If it's the last hotspot, go to the full view of the next page
+					// If that exceeds the number of hotspots on this page, go to the full view of the next page
 					if (pp_info.hotspot > page_max){
 						pp_info.page++;
 						pp_info.hotspot = 'none';
 					}
 
+				// Go to the page view
+				} else if (direction == 'page-view'){
+					pp_info.hotspot = 'none';
 				}
 
 				// Add our new info to the hash
@@ -228,18 +261,31 @@
 		// Switches pages if necessary
 		// Scales to page view if no hotspot is set
 		// Delegates zooms to hotspot if it is
-		read: function(page, hotspot){
+		read: function(page, hotspot, transitionDuration){
 			var css;
+			var page_change_direction, exiting_class, entering_class;
+			// If we're changing pages
 			if (states.currentPage != page){
-				// Load that page
-				// TK page navigation and possible async headache
+				if ( Number(states.currentPage) < Number(page) ) {
+					page_change_direction = 'next-page';
+					exiting_class = 'exit-to-left';
+					entering_class = 'enter-from-right';
+				} else {
+					page_change_direction = 'prev-page';
+					exiting_class = 'exit-to-right';
+					entering_class = 'enter-from-left';
+				}
+				console.log(states.currentPage, page, page_change_direction)
+
+				$('#page-'+states.currentPage).addClass(exiting_class);
+				$('#page-'+page).addClass(entering_class).addClass('viewing');
 			}
 
 			if (!hotspot){
 				// Reset back to full page view here
-				zooming.reset(page);
+				zooming.toPage( $('#page-'+page), true , true);
 			}else{
-				zooming.toHotspot(page, hotspot);
+				zooming.toHotspot(page, hotspot, transitionDuration);
 			}
 
 			// Save the current page and hotspot to what the route said. TK placement here. I'll have to see how all the nav mixures play out and how pagination works
@@ -248,15 +294,17 @@
 	}
 
 	var zooming = {
-		reset: function(page){
+		toPage: function($page, masksAlso, transitionDuration){
 			// Reset zoom to full page view
-			var page_css = helpers.setTransitionCss('transform', 'scale(1)');
-			$('#page-'+page).css(page_css);
-			// Reset masks
-			var mask_css = helpers.addDuration({ 'height': 0, opacity: 0 })
-			$('.mask').css(mask_css);
+			var page_css = helpers.setTransitionCss('transform', 'scale(1)', transitionDuration);
+			$page.css(page_css);
+			if (masksAlso){
+				// Reset masks
+				var mask_css = helpers.addDuration({ 'height': 0, opacity: 0 }, transitionDuration);
+				$('.mask').css(mask_css);
+			}
 		},
-		toHotspot: function(page, hotspot){
+		toHotspot: function(page, hotspot, transitionDuration){
 			// cg means `current page`
 			// th means `target hotspot`
 			var buffer = .2;
@@ -276,15 +324,15 @@
 			var scale_multiplier = 1 / (th_width / cg_width); // Scale the width of the page by this to expand the target hotspot to full view
 			var x_adjuster = viewport_xMiddle - th_left - th_xMiddle,
 					y_adjuster = cg_yMiddle - th_top - th_yMiddle;
-
-			var css = helpers.setTransitionCss('transform', 'scale('+ scale_multiplier +') translate('+x_adjuster+'px, '+y_adjuster+'px)')
+			console.log(cg_yMiddle, th_top, th_yMiddle)
+			var css = helpers.setTransitionCss('transform', 'scale('+ scale_multiplier +') translate('+x_adjuster+'px, '+y_adjuster+'px)', transitionDuration);
 			$currentPage.css(css);
-			zooming.sizeMasks(th_yMiddle*2, cg_yMiddle*2, scale_multiplier);
+			zooming.sizeMasks(th_yMiddle*2, cg_yMiddle*2, scale_multiplier, transitionDuration);
 		},
-		sizeMasks: function(th_height, cg_height, scaler){
+		sizeMasks: function(th_height, cg_height, scaler, transitionDuration){
 			var mask_height = ( cg_height - (th_height * scaler) ) / 2;
 			var css = { 'height': mask_height+'px', opacity: 1 };
-			css = helpers.addDuration(css)
+			css = helpers.addDuration(css, transitionDuration)
 			$('.mask').css(css);
 		}
 	}
