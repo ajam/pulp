7931733a16981c8bdee979de12483f7975c5951c
mhkeller <michael.keller@gmail.com>
2014-07-12 00:32:37 -0400

    full screen toggle

--- js/app.js
+++ js/app.js
@@ -88,7 +88,26 @@
 				return false;
 			}
 			return false;
-		}	
+		},
+		toggleFullScreen: function(){
+		  if ((document.fullScreenElement && document.fullScreenElement !== null) || (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+		    if (document.documentElement.requestFullScreen) {  
+		      document.documentElement.requestFullScreen();  
+		    } else if (document.documentElement.mozRequestFullScreen) {  
+		      document.documentElement.mozRequestFullScreen();  
+		    } else if (document.documentElement.webkitRequestFullScreen) {  
+		      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+		    }
+		  } else {  
+		    if (document.cancelFullScreen) {  
+		      document.cancelFullScreen();  
+		    } else if (document.mozCancelFullScreen) {  
+		      document.mozCancelFullScreen();  
+		    } else if (document.webkitCancelFullScreen) {  
+		      document.webkitCancelFullScreen();  
+		    }  
+		  }  
+		}
 	}
 
 	var templates = {
