31a6fda28b46edacaca202c70073ce776317fbf5
mhkeller <michael.keller@gmail.com>
2015-11-30 11:45:01 -0500

    minify js

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,36 @@ var plumber = require('gulp-plumber')
 var nib = require('nib')
 var minimist = require('minimist')
 var gls = require('gulp-live-server')
-var jsonStylus = require('gulp-json-stylus');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
 
 var argv = minimist(process.argv.slice(2))
 
 var paths = {
   config: 'config.json',
+  gulpfile: 'gulpfile.js',
   js: {
-    main: 'js/main.js'
+    main: 'js/main.js',
+    thirdparty: [
+      'jquery-1.11.0.min.js',
+      'underscore-min.js',
+      'backbone-min.js',
+      'imagesloaded.pkgd.min.js',
+      'jquery.detectSwipe.js',
+      'scrollfix.js',
+      'fastclick.js',
+      'sideburns.js'
+    ].map(function(lib){ return 'js/thirdparty/' + lib }),
+    allThirdparty: 'js/thirdparty/*.js'
   },
   css : {
     styl: 'css/styles.styl'
     // css: 'css/styles.css'
   }
-};
+}
+
+paths.js.all = paths.js.thirdparty.concat([paths.js.main])
 
 // paths.all = [paths.js.main, paths.css.styl, 'index.html', 'imgs/**/*', 'data/pages.json', 'config.json']
 
@@ -39,9 +55,21 @@ gulp.task('compile-stylus', function() {
     .pipe(gulp.dest('css/'))
 })
 
+gulp.task('make-js-pkg', function() {
+  return gulp.src(paths.js.all)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(uglify({
+      preserveComments: 'some'
+    }))
+    .pipe(concat('main.pkgd.min.js')) // The file to write out
+    .pipe(sourcemaps.write('./')) // Write these in the same folder as our `dest` in the line below
+    .pipe(gulp.dest('./js/'));
+});
+
 gulp.task('watch', function() {
-  gulp.watch(paths.css.styl, ['compile-stylus'])
+  gulp.watch([paths.gulpfile, paths.config, paths.css.styl, paths.js.main, paths.js.allThirdparty], ['compile-stylus', 'make-js-pkg'])
 });
 
 gulp.task('default', ['compile-stylus']); // Simply compile
-gulp.task('watch-files',   ['watch', 'compile-stylus', 'serve']); // Watch files for changes
+gulp.task('watch-files',   ['watch', 'compile-stylus', 'make-js-pkg', 'serve']); // Watch files for changes
